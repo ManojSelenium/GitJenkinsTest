fd2cbe66de51728ac1b4b5346c3f2b2bf0732ef8
ManojSelenium <manojqa5006@gmail.com>
2016-07-06 09:49:04 +0530

    Jenkins

---
+++ JenkinsProject/src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,32 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	
+	@Test
+	public void tc_1() {
+		System.out.println("First Test Case");
+	}
+	
+	@Test
+	public void tc_2() {
+		System.out.println("Second Test Case");
+	}
+	
+	@Test
+	public void tc_3() {
+		System.out.println("Third Test Case");
+	}
+	
+	@Test
+	public void tc_4() {
+		System.out.println("Fourth Test Case");
+	}
+	
+	@Test
+	public void tc_5() {
+		System.out.println("Fifth Test Case");
+	}
+}
